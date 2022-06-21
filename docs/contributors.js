a2a6280235bfb96dc1a98734b164b17bdbffbce1
Cesar Hernandez <cfhernan123@gmail.com>
2022-06-21 14:18:34 -0600

    updated docs/contributos.js

--- docs/contributors.js
+++ docs/contributors.js
@@ -15,7 +15,7 @@
 (function (jQuery) {
     // contributors list source Url
   const contributorsUrl =
-    "https://microprofile.github.io/microprofile-site/contributors.json";
+    "https://cesarhernandezgt.github.io/mp-bot/contributors.json";
 
   const scripts = [
     "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.3/angular.min.js",
@@ -29,7 +29,7 @@
   // to load in parallel use Promise.all(scripts.map(promiseGetScript))
   const arrayWaterfall = (array, fn) =>
     array.reduce((p, script) => p.then(() => fn(script)), Promise.resolve());
-  
+
   // check if element with id `microprofileio-contributors-div` exists
   const bootstrapEl = document.getElementById(
     "microprofileio-contributors-div"
